e5976dc4c48ca451a86d518e05c2c637d7da82b7
AsadullahBadhan <asadullah.badhan@gmail.com>
2021-09-01 02:39:46 +0600

    renaming and comment

--- longest-subsequence.js
+++ longest-subsequence.js
@@ -1,6 +1,7 @@
 // This is a former challenging Google interview question. The task is finding the 'longest Subsequence' of any given 'string' from a dictionary(array of strings).
 
-// A string can be a subseqence of another string if all of its characters appear and are in the same order.For example, 'hello' is a subseqence of 'help love everyone' because 'h', 'e', 'l', 'l' and 'o' appear in the second string in the same order as 'hello'.
+// A string can be a subseqence of another string if all of its characters appear and are in the same order.
+// For example, 'hello' is a subseqence of 'help love everyone' because 'h', 'e', 'l', 'l' and 'o' appear in the second string in the same order as 'hello'.
 // 'abe' is not a subseqence of 'bale', because the 'a', 'b', and 'e' are not in the same order.
 // 'bus' is a subseqence of 'abacus' because the 'b', 'u', and 's' all appear in the same order in 'abacus'.
 
@@ -9,6 +10,7 @@
 // To solve this problem we have to break it down. firstly we will make some helper function. 
 
 const dictionary = ['js', 'java', 'javas', 'javast', 'kangaroo'];
+const word = 'javascript';
 
 //finding the longest word from an array of string
 function longestWord(subseqences) {
@@ -39,11 +41,11 @@ function mapString(word) {
 
 //this function takes a string(word) and an object. ckeck if each letter in string exists as a
 //property in map we have just created
-function isSubsequence(word, object) {
+function isSubsequence(word, map) {
     let index = 0;
     for (const letter of word) {
-        if (object[letter]) {
-            index = findNextIndex(object[letter], index);
+        if (map[letter]) {
+            index = findNextIndex(map[letter], index);
             if (index === false) {
                 return false;
             }
@@ -57,16 +59,15 @@ function isSubsequence(word, object) {
 
 //this helper function will compare the index position of the word and subsequences
 function findNextIndex(array, minIndex) {
-    for (const elemnet of array) {
-        if (elemnet >= minIndex) {
-            return elemnet + 1;
+    for (const index of array) {
+        if (index >= minIndex) {
+            return index + 1;
         }
     }
     return false;
 }
 
-//So now we have all the helper function. Let's put all puzle pices together and make the final function of
-//our problem
+//So now we have all the helper function. Let's put all puzle pices together and make the final function of our problem
 function longestSubsequence(word, dictionary) {
     let map = mapString(word);
     let subsequences = [];
